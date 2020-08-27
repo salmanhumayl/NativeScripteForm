f4a07033f4751880d8878db00f71bb028d70eed8
Salman Mazhar <smazhar@ajes.ae>
2020-08-27 17:25:48 +0400

    images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,22 +2,33 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
+import {Page, EventData} from 'tns-core-modules/ui/page';
+import {RouterExtensions} from "@nativescript/angular/router";
+
 @Component({
     selector: "Home",
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    constructor() {
+    constructor(private page:Page, private routerExt:RouterExtensions) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
+        this.page.backgroundImage="http://portal.ajes.ae/epro/ajesbg.png";
         // Init your component properties here.
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    onItemTap(arg)
+    {
+        this.routerExt.navigate(["/pendings"], { clearHistory: false });
+
     }
 }
